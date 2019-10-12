dbc360ee6bab005f83f276a5cafa090922b166da
sasumaki <sasu.makinen@helsinki.fi>
2019-10-12 14:35:36 +0300

    päivityssssss

--- server/app.py
+++ server/app.py
@@ -11,6 +11,7 @@ import imageio
 from keras import backend
 from flask_cors import CORS, cross_origin
 from io import BytesIO
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -23,7 +24,7 @@ def ping():
 @app.route('/kurkkuvaimopo', methods=["POST"])
 def test():
   data = request.files.get('img', '')
-  data = imageio.imread(data)
+  data = imageio.imread(data, pilmode="RGB")
   data = imresize(data, (128,128))
   with backend.get_session().graph.as_default() as g:
       res = model.predict(np.array(data).reshape(1, 128, 128, 3))
@@ -31,9 +32,23 @@ def test():
 
 
 if __name__ == '__main__':
+  print("Backend starting")
   load_dotenv()
+  if not os.path.isdir("./model"):
+    print("NO MODEL VOLUME")
+    exit(1)
   debug = os.getenv('ENV') == 'development'
-  global model 
-  model = pickle.load(open('./model/kurkkumopotin.sav', 'rb'))
+  global model
+  boolean_print = False
+  while True:
+    try:
+      model = pickle.load(open('./model/kurkkumopotin.sav', 'rb'))
+      break
+    except:
+      if not boolean_print:
+        print("No 'kurkkumopotin.sav' model in the model volume. Waiting for training service to provide one.")
+        boolean_print = True
+      time.sleep(5)
+      continue
 
   app.run("0.0.0.0", port=5000, debug=debug)
